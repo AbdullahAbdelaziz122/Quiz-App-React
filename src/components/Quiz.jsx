807e61e3c21f1c8e837031ff581edf41fbcf6d33
abdullahabdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-11-13 20:48:58 +0200

    Re arrange Quiz component after seperation

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,84 +1,41 @@
-import { useCallback, useRef, useState } from "react";
-import Questions from "../questions.js";
-import quizComplete from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
-import Answer from "./Answers.jsx";
+import { useState, useCallback } from 'react';
+
+import QUESTIONS from '../questions.js';
+import Question from './Question.jsx';
+import Summary from './Summary.jsx';
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const [answerState, setAnswerState] = useState("");
-  const currentQuestion =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
-  
 
-  const handleSelectedAnswer = useCallback(
-    function handleSelectedAnswer(selectedAnswer) {
-      setAnswerState("answered");
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-      setUserAnswers((prevAnswers) => {
-        return [...prevAnswers, selectedAnswer];
-      });
-
-      setTimeout(() => {
-        if (selectedAnswer === Questions[currentQuestion].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [currentQuestion]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
 
   const handleSkipAnswer = useCallback(
-    () => handleSelectedAnswer(null),
-    [handleSelectedAnswer]
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
   );
 
-  /*
-      - handle end of the question
-      - if questions ended preview summary screen
-    */
-  const quizEnded = currentQuestion === Questions.length;
-  if (quizEnded) {
-    return (
-      <div id="summary">
-        <img src={quizComplete} alt="Quiz summary picture" />
-        <h2>Quiz is complete</h2>
-      </div>
-    );
-  }
-
-  let timer = 10000;
-  if(answerState === "answered"){
-    timer = 2000;
-  }
-
-  if (answerState === "correct" || answerState === "wrong"){
-    timer = 2000;
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />
   }
 
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={currentQuestion}
-          timeout={timer}
-          onTimeout={answerState === "" ? handleSkipAnswer : null}
-        />
-        <h1>{Questions[currentQuestion].text}</h1>
-        <main> <Answer
-                key={currentQuestion}
-                answers={Questions[currentQuestion].answers}
-                selectedAnswer={userAnswers[userAnswers.length - 1]}
-                answerState={answerState}
-                onSelect={handleSelectedAnswer}
-                
-                /> </main>
-      </div>
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
